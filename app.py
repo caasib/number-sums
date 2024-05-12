7656247006f0dc548cb25451d726f7fc2ee0e5aa
caasib <cboakye@purdue.edu>
2024-05-12 16:04:08 -0400

    randomized row values

--- app.py
+++ app.py
@@ -23,19 +23,27 @@ class App:
             grid_row = []
             grid_row.append(Cell(0, row * self.rect_height, self.rect_width, self.rect_height, str(row_sum)))
             remaining_sum = row_sum
-            
-            for col in range(1, self.grid_columns):
+            cell_values = []
+
+            for _ in range(1, self.grid_columns):
                 if remaining_sum <= 0:
                     text = str(random.randint(1, 20))
+                    cell_values.append(text)
                 else:
                     text = str(random.randint(1, remaining_sum))
+                    cell_values.append(text)
                     remaining_sum -= int(text)
+            
+            random.shuffle(cell_values)
+
+            for col, val in enumerate(cell_values, start=1):
                 x = col * self.rect_width
                 y = row * self.rect_height
-                grid_row.append(Cell(x, y, self.rect_width, self.rect_height, text))
+                grid_row.append(Cell(x, y, self.rect_width, self.rect_height, val))
             
             if (row == 0 and row_sum == row_sums[0]):
                 grid_row.pop(0)
+            
             self.grid.append(grid_row)
 
         self.running = True
